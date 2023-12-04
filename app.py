cf27fb9ad5fbc04e86d3e7fbc9d4035a9dc510d8
DHANUSHSRINI <131330164+DHANUSHSRINI@users.noreply.github.com>
2023-12-05 01:55:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,183 @@
+import streamlit as st
+import nltk
+import spacy
+from pyresparser import ResumeParser
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import TextConverter
+import io, random
+from streamlit_tags import st_tags
+from PIL import Image
+import base64, time, datetime
+
+nltk.download('stopwords')
+spacy.cli.download("en_core_web_sm")
+spacy.load('en_core_web_sm')
+
+
+def pdf_reader(file):
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+    with open(file, 'rb') as fh:
+        for page in PDFPage.get_pages(fh,
+                                      caching=True,
+                                      check_extractable=True):
+            page_interpreter.process_page(page)
+            print(page)
+        text = fake_file_handle.getvalue()
+
+    # close open handles
+    converter.close()
+    fake_file_handle.close()
+    return text
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+def run():
+    st.title("Smart Resume Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["Normal User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+    img = Image.open('SRA_Logo.ico')
+    img = img.resize((250, 250))
+    st.image(img)
+
+    if choice == 'Normal User':
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            save_image_path = './Uploaded_Resumes/' + pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                resume_text = pdf_reader(save_image_path)
+
+                st.header("**Resume Analysis**")
+                st.success("Hello " + resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: ' + resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: ' + str(resume_data['no_of_pages']))
+                except:
+                    pass
+
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',
+                                unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >= 3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',
+                                unsafe_allow_html=True)
+
+                st.subheader("**Skills Recommendation💡**")
+                keywords = st_tags(label='### Skills that you have',
+                                   text='See our skills recommendation',
+                                   value=resume_data['skills'], key='1')
+
+                ds_keyword = ['tensorflow', 'keras', 'pytorch', 'machine learning', 'deep Learning', 'flask',
+                              'streamlit']
+                web_keyword = ['react', 'django', 'node jS', 'react js', 'php', 'laravel', 'magento', 'wordpress',
+                               'javascript', 'angular js', 'c#', 'flask']
+                android_keyword = ['android', 'android development', 'flutter', 'kotlin', 'xml', 'kivy']
+                ios_keyword = ['ios', 'ios development', 'swift', 'cocoa', 'cocoa touch', 'xcode']
+                uiux_keyword = ['ux', 'adobe xd', 'figma', 'zeplin', 'balsamiq', 'ui', 'prototyping', 'wireframes',
+                                'storyframes', 'adobe photoshop', 'photoshop', 'editing', 'adobe illustrator',
+                                'illustrator', 'adobe after effects', 'after effects', 'adobe premier pro',
+                                'premier pro', 'adobe indesign', 'indesign', 'wireframe', 'solid', 'grasp',
+                                'user research', 'user experience']
+
+                recommended_skills = []
+
+                st.subheader("**Resume Tips & Ideas💡**")
+                resume_score = 0
+                if 'Objective' in resume_text:
+                    resume_score += 20
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Objective</h4>''',
+                                unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add your career objective, it will give your career intention to the Recruiters.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Declaration' in resume_text:
+                    resume_score += 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added Declaration✍/h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Declaration✍. It will give the assurance that everything written on your resume is true and fully acknowledged by you</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Hobbies' or 'Interests' in resume_text:
+                    resume_score += 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Hobbies⚽</h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Hobbies⚽. It will show your personality to the Recruiters and give the assurance that you are fit for this role or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Achievements' in resume_text:
+                    resume_score += 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Achievements🏅 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Achievements🏅. It will show that you are capable of the required position.</h4>''',
+                        unsafe_allow_html=True)
+
+                if 'Projects' in resume_text:
+                    resume_score += 20
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #1ed760;'>[+] Awesome! You have added your Projects👨‍💻 </h4>''',
+                        unsafe_allow_html=True)
+                else:
+                    st.markdown(
+                        '''<h4 style='text-align: left; color: #fabc10;'>[-] According to our recommendation please add Projects👨‍💻. It will show that you have done work related to the required position or not.</h4>''',
+                        unsafe_allow_html=True)
+
+                st.subheader("**Resume Score📝**")
+                st.markdown(
+                    """
+                    <style>
+                        .stProgress > div > div > div > div {
+                            background-color: #d73b5c;
+                        }
+                    </style>""",
+                    unsafe_allow_html=True,
+                )
+                my_bar = st.progress(0)
+                score = 0
+                for percent_complete in range(resume_score):
+                    score += 1
+                    time.sleep(0.1)
+                    my_bar.progress(percent_complete + 1)
+                st.success('** Your Resume Writing Score: ' + str(score) + '**')
+                st.warning(
+                    "** Note: This score is calculated based on the content that you have added to your Resume. **")
+                st.balloons()
+
+
+run()
